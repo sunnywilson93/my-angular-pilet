42f5716efc4bea52c63b7285f67a353558dc9e3e
Wilson <nc23203-wilson@ninja-its-MacBook-Pro-2.local>
2022-05-25 11:24:35 +0530

    initial commit

---
+++ src/app/second.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: ` <h1>More Routing</h1>
+    <ul>
+      <li><a routerLink="/one">First page.</a></li>
+      <li><a routerLink="/bar">React page.</a></li>
+    </ul>`
+})
+export class SecondPageComponent {}
